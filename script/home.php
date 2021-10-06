fd4f206be3ec6f8b317cb831b86a66392b61d71d
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2021-10-06 17:17:48 +0600

    Delete Button Added in Vehicale Page

--- script/home.php
+++ script/home.php
@@ -5,3 +5,18 @@ namespace sPHP;
 
 echo "THIS IS HOME PAGE";
 ?>
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../image/icon/car.png" alt="Avatar" style="width:300px;height:300px;">
+     <a href="">
+     <button class="btn btn-primary">Book Vehicle</button>
+
+     </a>
+</div>
+    <div class="flip-card-back">
+      <h1>Welcome To Our Vehicle Managment System</h1>
+    
+    </div>
+  </div>
+</div>
