d091307374a467afa0dc12e8b6ef6917c2449eeb
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2021-10-06 14:03:42 +0600

    Update leftpanelnavigation.php

--- template/leftpanelnavigation.php
+++ template/leftpanelnavigation.php
@@ -11,6 +11,7 @@ $LeftPanelLinkHTML[] = HTML\UI\Accordion("LeftPanelNavigation", [
 		new HTML\UI\Accordion\Item($Caption = "Address", null, $Application->URL("Management/Generic/Address"), null, null, "" . strtolower("" . ($PadKey = "Administration") . "_" . ($Key = "Address")) . "", "{$Caption}", null, "{$Key}"),
 		new HTML\UI\Accordion\Item($Caption = "Phone", null, $Application->URL("Management/Generic/Phone"), null, null, "" . strtolower("" . ($PadKey = "Administration") . "_" . ($Key = "Phone")) . "", "{$Caption}", null, "{$Key}"),
 		new HTML\UI\Accordion\Item($Caption = "Note", null, $Application->URL("Management/Generic/Note"), null, null, "" . strtolower("" . ($PadKey = "Administration") . "_" . ($Key = "note")) . "", "{$Caption}", null, "{$Key}"),
+		new HTML\UI\Accordion\Item($Caption = "Route", null, $Application->URL("Management/Generic/Route"), null, null, "" . strtolower("" . ($PadKey = "Administration") . "_" . ($Key = "route")) . "", "{$Caption}", null, "{$Key}"),
 		new HTML\UI\Accordion\Item($Caption = "Just Fun", null, $Application->URL("Management/Generic/JustFun"), null, null, "" . strtolower("" . ($PadKey = "Administration") . "_" . ($Key = "Fun")) . "", "{$Caption}", null, "{$Key}"),
 		
 	], "" . ($Caption = "Administration") . "", "{$Caption}", "{$Caption}", null, "{$PadKey}") : null,
